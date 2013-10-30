6293c2958f2c8e3ae3eedc64e2aec35a71d97027
monsGT-R <monsgt-r@yandex.ru>
2013-10-30 23:48:50 +0800

    Create minisessia.cpp

---
+++ minisessia.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Введите N: ";
+	cin >> n;
+	while (n < 1)
+	{
+		cout << "[X] Введите положительное число N: ";
+		cin >> n;
+	}
+	cout << "Введите " << n << " целых чисел" << endl;
+	int a, min,
+		mod3 = 0,
+		proizvedenie = 1,
+		primes = 0;
+	for (int i = 0, j; i < n; i++)
+	{
+		cin >> a;
+		if (a % 3 == 0) mod3++;
+		if (a > 0) proizvedenie *= a;
+		if (a >= 2) {
+			for (j = 2; (j < a) && (a % j != 0); j++)
+				;
+			if (a == j) primes++;
+		}
+		if ((i == 0) || (min > a)) min = a;
+	}
+	cout << "Количество чисел кратных 3 = " << mod3 << endl;
+	cout << "Произведение неотрицательных чисел = " << proizvedenie << endl;
+	cout << "Количество простых чисел = " << primes << endl;
+	cout << "Минимальное число = " << min << endl;
+}
